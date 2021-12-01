81f76b0a88436d66bd88a6e1881417116478177a
Zan Skamljic <zan.skamljic@equaleyes.com>
2021-12-01 06:56:30 +0100

    feat: Add Day 01 part 1

--- src/Day01.java
+++ src/Day01.java
@@ -1,5 +1,23 @@
-public class Day01 {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Day01 {
+    public static void main(String[] args) throws IOException {
+        var lines = Files.readAllLines(Paths.get("input01.txt"))
+            .stream()
+            .map(Integer::parseInt)
+            .toList();
+
+        var increases = 0;
+        var previous = lines.get(0);
+        for(int i = 1; i< lines.size();i++) {
+            var current = lines.get(i);
+            if (current > previous) {
+                increases++;
+            }
+            previous = current;
+        }
+        System.out.println(increases);
     }
 }
