40c51655888919219f8d7ccde5516b7502c9af4a
Zan Skamljic <zan.skamljic@equaleyes.com>
2021-12-07 06:46:47 +0100

    feat: add Day 07

---
+++ src/Day07.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.function.IntFunction;
+
+public class Day07 {
+    public static void main(String[] args) throws IOException {
+        var input2 = "16,1,2,0,4,2,7,1,2,14";
+        var input = Files.readString(Paths.get("input07.txt")).trim();
+
+        var positions = Arrays.stream(input.split(","))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        Arrays.sort(positions);
+
+        part01(positions);
+        part02(positions);
+    }
+
+    private static void part01(int[] positions) {
+        calculateMinFuel(positions, i -> i);
+    }
+
+    private static void part02(int[] positions) {
+        calculateMinFuel(positions, Day07::calculateCost);
+    }
+
+    private static void calculateMinFuel(int[] positions, IntFunction<Integer> costing) {
+        var minFuel = Integer.MAX_VALUE;
+        for (var i = 0; i < positions[positions.length - 1]; i++) {
+            int finalI = i;
+            var fuel = Arrays.stream(positions)
+                .map(x -> costing.apply(Math.abs(x - finalI)))
+                .sum();
+            if (fuel < minFuel) {
+                minFuel = fuel;
+            }
+        }
+        System.out.println(minFuel);
+    }
+
+    private static int calculateCost(int distance) {
+        return distance * (distance + 1) / 2;
+    }
+}
