ac6848b9bcbdc0d555393a1ada63fb92791f6373
Zan Skamljic <zan.skamljic@equaleyes.com>
2021-12-20 19:36:04 +0100

    feat: optimize day 20

--- src/Day20.java
+++ src/Day20.java
@@ -2,20 +2,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day20 {
     public static void main(String[] args) throws IOException {
-        var input = """
-            ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
-                        
-            #..#.
-            #....
-            ##..#
-            ..#..
-            ..###
-            """;
-        input = Files.readString(Paths.get("input20.txt"));
+        var input = Files.readString(Paths.get("input20.txt"));
 
         var parts = input.split("\n\n");
         var enhancementAlgorithm = parts[0].toCharArray();
@@ -25,78 +17,64 @@ public class Day20 {
         part2(image, enhancementAlgorithm);
     }
 
-    private static void part1(List<Point> image, char[] enhancementAlgorithm) {
-        enhance(image, 2, enhancementAlgorithm);
+    private static void part1(List<String> image, char[] enhancementAlgorithm) {
+        enhanceTimes(image, 2, enhancementAlgorithm);
     }
 
-    private static void part2(List<Point> image, char[] enhancementAlgorithm) {
-        enhance(image, 50, enhancementAlgorithm);
+    private static void part2(List<String> image, char[] enhancementAlgorithm) {
+        enhanceTimes(image, 50, enhancementAlgorithm);
     }
 
-    private static void enhance(List<Point> image, int times, char[] enhancementAlgorithm) {
+    private static void enhanceTimes(List<String> image, int times, char[] enhancementAlgorithm) {
         var darkPixel = '.';
         for (int i = 0; i < times; i++) {
             image = enhance(image, enhancementAlgorithm, darkPixel);
             darkPixel = enhancementAlgorithm[darkPixel == '.' ? 0 : enhancementAlgorithm.length - 1];
         }
-        System.out.println(image.size());
+        var lit = image.stream()
+            .map(s -> s.replaceAll("[^#]", ""))
+            .mapToInt(String::length)
+            .sum();
+        System.out.println(lit);
     }
 
-    private static List<Point> enhance(List<Point> image, char[] enhancementAlgorithm, char darkPixel) {
-        var minX = Integer.MAX_VALUE;
-        var minY = Integer.MAX_VALUE;
-        var maxX = Integer.MIN_VALUE;
-        var maxY = Integer.MIN_VALUE;
-
-        for (var point : image) {
-            if (point.x < minX) {
-                minX = point.x;
-            }
-            if (point.y < minY) {
-                minY = point.y;
-            }
-            if (point.x > maxX) {
-                maxX = point.x;
-            }
-            if (point.y > maxY) {
-                maxY = point.y;
-            }
-        }
-        var result = new ArrayList<Point>();
-        for (int y = minY - 1; y <= maxY + 1; y++) {
-            for (int x = minX - 1; x <= maxX + 1; x++) {
+    private static List<String> enhance(List<String> image, char[] enhancementAlgorithm, char darkPixel) {
+        var result = new ArrayList<String>();
+        for (int y = -1; y <= image.size(); y++) {
+            var lineBuilder = new StringBuilder();
+            for (int x = -1; x <= image.get(0).length(); x++) {
                 var point = new Point(x, y);
-                var value = point.index(image, minX, minY, maxX, maxY, darkPixel);
+                var value = point.index(image, darkPixel);
                 if (enhancementAlgorithm[value] == '#') {
-                    result.add(point);
+                    lineBuilder.append("#");
+                } else {
+                    lineBuilder.append(".");
                 }
             }
+            result.add(lineBuilder.toString());
         }
         return result;
     }
 
-    private static List<Point> parseImage(String image) {
-        var result = new ArrayList<Point>();
-        var y = 0;
-        for (var line : image.split("\n")) {
-            for (int x = 0; x < line.length(); x++) {
-                if (line.charAt(x) == '#') {
-                    result.add(new Point(x, y));
-                }
-            }
-            y++;
-        }
-        return result;
+    private static List<String> parseImage(String image) {
+        return Arrays.asList(image.split("\n"));
     }
 
     record Point(int x, int y) {
-        public int index(List<Point> image, int minX, int minY, int maxX, int maxY, char darkPixel) {
+        public int index(List<String> image, char darkPixel) {
             var value = 0;
             for (var y = -1; y <= 1; y++) {
                 for (var x = -1; x <= 1; x++) {
                     value <<= 1;
                     var point = new Point(this.x + x, this.y + y);
-                    if (darkPixel == '#' && (point.x < minX || point.y < minY || point.x > maxX || point.y > maxY) || image.contains(point)) {
+                    var outside = point.x < 0 || point.y < 0 || point.y >= image.size() || point.x >= image.get(0).length();
+                    char pixel;
+                    if (outside) {
+                        pixel = darkPixel;
+                    } else {
+                        pixel = image.get(point.y).charAt(point.x);
+                    }
+                    if (pixel == '#') {
                         value |= 1;
                     }
                 }
